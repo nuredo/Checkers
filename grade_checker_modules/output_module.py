93c116dd6976296c09a7710e9f1342eaf1d30edc
Nurudeen <122872738+nuredo@users.noreply.github.com>
2026-05-03 17:56:40 +0100

    Add grade_checker_modules/output_module.py

---
+++ grade_checker_modules/output_module.py
@@ -0,0 +1,7 @@
+def display_result(name, score, grade, remark):
+    print("\n" + "=" * 35)
+    print(f"  Student Name : {name}")
+    print(f"  Score        : {score}")
+    print(f"  Grade        : {grade}")
+    print(f"  Remark       : {remark}")
+    print("=" * 35)
